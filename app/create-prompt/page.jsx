0b85504369d8600ff0c09af087095ef03f48b180
Chrispy <christiancarteno@hotmail.com>
2023-05-27 00:44:19 -0700

    added to create a prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,12 +4,26 @@ import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
-import Form from '@/components/form';
+import Form from '@/components/Form';
 
 const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post , setPost] = useState({
+    title: '',
+    content: '',
+    tags: [],
+  });
+
+  const CreatePrompt = async (e) => {
+    e.preventDefault();
+  }
   return (
     <Form
-    
+      type="create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={CreatePrompt}
     />
   )
 }
